06e8455074bf417c55e385f74ef3938abda6afc5
Bill Branan <bbranan@duraspace.org>
2013-02-14 10:43:05 -0500

    Updates the file header in the dfr-sync project to be consistent with the file headers in the other DfR projects.

--- dfr-sync/src/test/java/org/duraspace/dfr/sync/setup/SaveSetupActionTest.java
+++ dfr-sync/src/test/java/org/duraspace/dfr/sync/setup/SaveSetupActionTest.java
@@ -1,6 +1,11 @@
 /*
- * Copyright (c) 2009-2012 DuraSpace. All rights reserved.
- */package org.duraspace.dfr.sync.setup;
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
+package org.duraspace.dfr.sync.setup;
 
 import org.duraspace.dfr.sync.domain.DirectoryConfig;
 import org.duraspace.dfr.sync.domain.DirectoryConfigs;
